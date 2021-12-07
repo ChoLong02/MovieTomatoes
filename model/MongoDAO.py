91cab03617d3bb0f2a64e825fa6e99a103be07b3
cholong <sentilemon02@gmail.com>
2021-12-07 16:17:46 +0900

    학습된 AI모델을 활용해서 긍부정 분석 완료

--- model/MongoDAO.py
+++ model/MongoDAO.py
@@ -7,10 +7,10 @@ from pymongo import MongoClient
 # MongoDB Connection
 def conn_mongo():
     client = MongoClient('localhost', 27017)  # (IP address, Port)
-    client = MongoClient(host='host',
-                         port=27017,
-                         username='user',
-                         password='password')
+    # client = MongoClient(host='host',
+    #                      port=27017,
+    #                      username='user',
+    #                      password='password')
     db = client['local']                      # Allocating 'local' DB
     collection = db.get_collection('movie')   # Allocating 'movie' Collection
     return collection
@@ -24,4 +24,20 @@ def add_review(data):
 
 # Select review data(데이터 조회)
 def get_reviews():
-    pass
+    collection = conn_mongo()  # MongoDB Connection
+    review_list = []
+    for one in collection.find({}, {'_id': 0, 'title':1, 'review':1, 'score':1}):  # 제목, 리뷰, 평점만 DB에서 조회
+        review_list.append([one['title'], one['review'], one['score']])
+    return review_list
+
+
+
+
+
+
+
+
+
+
+
+
